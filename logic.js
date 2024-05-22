31ff43d3399af8e60455ec13583649bcad9cbebe
Irina <131261791+IrinaGatalaiciuc@users.noreply.github.com>
2024-05-22 10:52:31 +0300

    add calendar - next to add todoList

--- logic.js
+++ logic.js
@@ -1,8 +1,11 @@
 const calendar = document.querySelector(".calendar"),
 date = document.querySelector(".date"),
 daysContainer = document.querySelector(".days"),
-prev = focument.querySelector(".prev"),
+prev = document.querySelector(".prev"),
 next = document.querySelector(".next")
+todayBtn = document.querySelector(".today-btn"),
+gotoBtn = document.querySelector(".goto-btn"),
+dateInput = document.querySelector(".date-input")
 
 let today = new Date();
 let activeDay;
@@ -68,3 +71,75 @@ for (let j = 1; j < nextDays; j++) {
 daysContainer.innerHTML = days;
 }
 initCalendar();
+
+//prev month 
+
+function prevMonth() {
+    month --;
+    if (month < 0) {
+        month = 11;
+        year--;
+    }
+    initCalendar();
+}
+
+//next month
+
+function nextMonth() {
+    month ++;
+    if (month > 11) {
+        month = 0;
+        year++;
+    }
+    initCalendar();
+}
+
+// add eventListener for prev and next
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+//for todo add
+
+todayBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+})
+
+dateInput.addEventListener("input", (e) => {
+//allow only numbers and remove anything else
+dateInput.value = dateInput.value.replace(/[^0-9/]/g, "")
+if (dateInput.value.length === 2) {
+    dateInput.value += "/"
+}
+if (dateInput.value.length > 7) {
+    dateInput.value = dateInput.value.slice(0, 7);
+}
+
+if (e.inputType === "deleteContentBackward") {
+    if (dateInput.value.length === 3) {
+        dateInput.value = dateInput.value.slice(0, 2);
+    }
+}
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+const dateArr = dateInput.value.split("/");
+
+if (dateArr.length === 2) {
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return
+        }
+    }
+}
+
+alert("invalid date")
+}
